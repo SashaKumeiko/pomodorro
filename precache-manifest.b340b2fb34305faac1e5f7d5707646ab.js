272f2f5d836408c281bc12c52f2d316288bf7b49
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-07-19 10:52:04 +0300

    Updates

---
+++ precache-manifest.b340b2fb34305faac1e5f7d5707646ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702f8d6abbf9a47d646ab4e59c717958",
+    "url": "/pomodorro/index.html"
+  },
+  {
+    "revision": "3b936fb7d4b0e4638a6b",
+    "url": "/pomodorro/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d7863093cdda9928ec3a",
+    "url": "/pomodorro/static/js/2.3d1f2605.chunk.js"
+  },
+  {
+    "revision": "82fd7b3faa9aef241966589e85d1bfbd",
+    "url": "/pomodorro/static/js/2.3d1f2605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b936fb7d4b0e4638a6b",
+    "url": "/pomodorro/static/js/main.1cf8acb0.chunk.js"
+  },
+  {
+    "revision": "4aa015ad3e0725ca9ad9",
+    "url": "/pomodorro/static/js/runtime-main.55e5e94f.js"
+  }
+]);
